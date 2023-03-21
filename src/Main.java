88af6eb141939ab2f2d24188fabf61d1f57fd0f4
safonov yurii <safonov-yurii89@mail.ru>
2023-03-21 20:17:21 +0700

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int score = 100;
+        boolean data = true;
+
+
+        int amount = 1700;
+
+
+        int bonus = (amount / 100);
+
+        if (amount < 1000) {
+            bonus = 0;
+        }
+
+        int result = (score + amount + bonus);
+
+        System.out.println("Итоговый баланс:" + result);
+
+
+    }
+}
